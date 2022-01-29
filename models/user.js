7a888f8b9c181212ebe39f4a1793ea6089844dad
mohaperu <mohaperu@gmail.com>
2022-01-29 19:44:00 +0530

    push

---
+++ models/user.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const Joi = require('joi');
+const passwordComplexity = require('joi-password-complexity');
+
+const userSchema = new mongoose.Schema({
+    username: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true }
+})
+
+// add mehod which returns json web token for specified user with the payload of id
+
+userSchema.methods.generateAuthToken = function () {
+    const token = jwt.sign(
+        { _id: this._id },
+        process.env.JWTPRIVATEKEY,
+        // {expiresIn:"7d"}
+    );
+
+    return token;
+}
+
+//user model
+
+const User = mongoose.model("user", userSchema);
+
+
+const validate = (data) => {
+    const schema = Joi.object({
+        username: Joi.string().required().label("username"),
+        email: Joi.string().email().required().label('Email'),
+        password: passwordComplexity().required().label("Password")
+    });
+    return schema.validate(data);
+}
+
+module.exports = {User,validate}
---
+++ routes/users.js
@@ -0,0 +1,29 @@
+//Register Route 
+const router = require('express').Router();
+const { User, validate } = require('../models/user');
+const bcrypt = require("bcrypt");
+
+router.post('/', async (req, res) => {
+    try {
+        const { error } = validate(req.body);
+        //check error
+        if (error) {
+            return res.status(400).send({ message: error.details[0].message })
+        }
+        //check email already exist or not
+        const user = await User.findOne({ email: req.body.email });
+        //if exist
+        if (user) {
+            return res.status(409).send({ message: "User with given email already exist" })
+        }
+        const salt = await bcrypt.genSalt(Number(process.env.SALT));
+        const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+        await new User({ ...req.body, password: hashedPassword }).save();
+        res.status(201).send({ message: "User created successfully" })
+    } catch (error) {
+        res.status(500).send({ message: "Internal Server Error" })
+    }
+})
+
+module.exports = router
